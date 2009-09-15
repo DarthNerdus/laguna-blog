c487c649d3d9c88bca9753ce7c07f4c1e2cd9c9a
Jesse Read <jread@me.com>
2009-09-15 17:15:00 -0400

    Updated archive_byline.php for new theme.
    Removed old date-based header.
    Opened post div for post.
    Print title and permalink.
    Open entry-content div for body text.

--- templates/archive_byline.php
+++ templates/archive_byline.php
@@ -1,6 +1,10 @@
 <?php
 
-echo "<h1>$date<div class=\"time\"><br />" . date($bylineTimeFormat, $moddate) . "</div>";
-echo "</h1>\n";
+echo "<div id=\"post\">";
+echo "<h2 class=\"entry-title\"><a href=\"$archiveURL/$archiveFilename\">$title</a>\n";
+echo "</h2>\n";
+echo "<div class=\"entry-type\"><a href=\"$archiveURL/$archiveFilename\"><img src=\"http://storage.dodgydev.net/public/text.gif\" alt=\"Permalink\"></a>
+</div>";
+echo "<div class=\"entry-content\">";
 
 ?>
