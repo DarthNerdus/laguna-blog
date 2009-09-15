c21473acecfa324cc2810c35976d2c9dff1327a6
Jesse Read <jread@me.com>
2009-09-15 17:11:41 -0400

    Cleaned up the header.php file with new layout.
    Removed line about not using without permission (it has been open sourced after all).
    Added line about my work.
    Removed unnecessary meta information from <head>.
    Updated <title> to be more generic.
    Cleaned CSS link.
    Made RSS link generic.
    Updated header styling to new theme.
    	Setup content for left column.
    	Opened container div for page contents.

--- templates/header.php
+++ templates/header.php
@@ -1,18 +1,32 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- Design and code copyright (C) 2008 Steven Frank. -->
-<!-- Please do not reuse without permission. -->
+<!-- Modified by Jesse Read. -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
-		<title>stevenf.com: <?php print $title; ?></title>
-		<meta name="ICBM" content="45.52, -122.67" />
-		<meta name="DC.title" content="stevenf.com" />
-		<meta name="description" content="Steven Frank discusses Macs, gadgets, and general geekery." />
+		<title>your name: <?php print $title; ?></title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link rel="stylesheet" type="text/css" href="<?php print $baseURL; ?>/css/default.css?2008091001" />
-		<link rel="alternate" type="application/rss+xml" title="RSS" href="http://stevenf.com/index.xml" />
+		<link rel="stylesheet" type="text/css" href="<?php print $baseURL; ?>/css/default.css" />
+		<link rel="alternate" type="application/rss+xml" title="RSS" href="http://yourdomain.com/index.xml" />
 	</head>
 	<body>
-		<div id="sitebanner"><a href="<?php print $baseURL; ?>">Laguna</a></div>
-		<div id="tagline">
-			<b>My new Laguna blog</b>
-		</div>
+		<div id="wrap">
+			<div id="left">
+				<h1>yourname</h1>
+				<div id="menu">
+					<ul>
+					<li><a href="/">Home</a></li>
+					<li><a href="/archive">Archive</a></li>
+					<li><a href="/index.xml">Feed</a></li>
+					</ul>
+				</div>
+				<div id="description">
+				All about you.
+				</div>
+				<div id="credit">
+				Designed by <a class="rubedo" href="http://redfield.tumblr.com">Redfield</a>. Icon by <a href="http://cameron.io/">Cameron Hunt</a>.
+				Copyright &copy; 2009 Me.
+				All rights reserved.
+				</div>
+			</div>
+
+			<div id="container">
